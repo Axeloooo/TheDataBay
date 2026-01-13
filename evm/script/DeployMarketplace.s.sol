2cea2b728831e4e85240676fb194bd58e7e39982
Axel Sanchez <axelshz@gmail.com>
2026-01-12 20:51:19 -0700

    feat: update DeployMarketplace script to use DEFAULT_ANVIL_KEY for Marketplace deployment

--- evm/script/DeployMarketplace.s.sol
+++ evm/script/DeployMarketplace.s.sol
@@ -5,6 +5,8 @@ import {Script} from "forge-std/Script.sol";
 import {Marketplace} from "../src/Marketplace.sol";
 
 contract DeployMarketplace is Script {
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     Marketplace public marketplace;
 
     function setUp() public {}
@@ -12,7 +14,7 @@ contract DeployMarketplace is Script {
     function run() public {
         vm.startBroadcast();
 
-        marketplace = new Marketplace();
+        marketplace = new Marketplace(vm.addr(DEFAULT_ANVIL_KEY));
 
         vm.stopBroadcast();
     }
